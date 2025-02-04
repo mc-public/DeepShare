b5f6e3e17a3cde84fe2f761e18f1764345f13989
mc-public <2625527057@qq.com>
2025-02-04 14:51:31 +0800

    Initial Commit

---
+++ ChatShare/ChatShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatShareApp.swift
+//  ChatShare
+//
+//  Created by 孟超 on 2025/2/4.
+//
+
+import SwiftUI
+
+@main
+struct ChatShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatShare/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatShare
+//
+//  Created by 孟超 on 2025/2/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
